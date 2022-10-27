5434a652213609317db35b19dd86fbc5fe69ead9
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2022-10-28 00:16:37 +0300

    :white_check_mark: use bigquery.Row instead of list for better mocking in CrimesLocationsDAL.crime_types test

--- app/tests/data_acess_layer/crimes_locations.py
+++ app/tests/data_acess_layer/crimes_locations.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 
 from google.cloud import bigquery
+from google.cloud.bigquery import Row
 
 from app.data_access_layer.crimes_locations import CrimeLocationsDAL
 
@@ -8,7 +9,10 @@ from app.data_access_layer.crimes_locations import CrimeLocationsDAL
 class TestCrimesLocationsDAL:
     def test_crime_types(self, mocked_session: bigquery.Client):
         mocked_session.query = MagicMock(
-            return_value=[{'primary_type': 'DID NOT WRITE TESTS'}, {'primary_type': 'DID NOT USE TYPING'}]
+            return_value=[
+                Row(('DID NOT WRITE TESTS',), {'primary_type': 0}),
+                Row(('DID NOT USE TYPING',), {'primary_type': 0})
+            ],
         )
 
         assert CrimeLocationsDAL(mocked_session).crime_types() == ['DID NOT WRITE TESTS', 'DID NOT USE TYPING']
