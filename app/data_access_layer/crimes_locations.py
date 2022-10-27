d5a73cb4308907688251057a5b40806293ed9a8a
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2022-10-28 00:09:17 +0300

    :sparkles: add method for fetching min and max dates from data access layer

--- app/data_access_layer/crimes_locations.py
+++ app/data_access_layer/crimes_locations.py
@@ -1,10 +1,12 @@
+import datetime
+
 from google.cloud import bigquery
+from more_itertools import one
 
 
 class CrimeLocationsDAL:
     def __init__(self, session: bigquery.Client):
         self.__session = session
-        # self.__session = session
 
     def crime_types(self) -> list[str]:
         """
@@ -19,3 +21,17 @@ class CrimeLocationsDAL:
         cursor = self.__session.query(query)
 
         return [row['primary_type'] for row in cursor]
+
+    def date_range(self) -> tuple[datetime.date, datetime.date]:
+        """
+        Returns first and last crime date from storage
+
+        >>> CrimeLocationsDAL(bigquery.Client()).date_range()
+        (datetime.date(2001, 1, 1), datetime.date(2022, 10, 18))
+
+        :return: Two dates, min and max accordingly
+        """
+        query = 'SELECT MIN(DATE(date)), MAX(DATE(date)) FROM `bigquery-public-data.chicago_crime.crime`;'
+        cursor = self.__session.query(query)
+
+        return tuple(one(cursor))
