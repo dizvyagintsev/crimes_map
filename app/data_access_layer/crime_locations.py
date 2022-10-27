36ed13bda670df1c5b86372ba12e4ee7ea4bd38e
Denis Zvyagintsev <dizvyagintsev@avito.ru>
2022-10-28 02:48:21 +0300

    :sparkles: add method for fetching crime locations to data access layer

--- app/data_access_layer/crime_locations.py
+++ app/data_access_layer/crime_locations.py
@@ -1,7 +1,20 @@
 import datetime
+from dataclasses import dataclass
 
 from google.cloud import bigquery
 from more_itertools import one
+
+
+@dataclass
+class DateRange:
+    start_date: datetime.date
+    end_date: datetime.date
+
+
+@dataclass
+class Location:
+    latitude: float
+    longitude: float
 
 
 class ChicagoCrimesDAL:
@@ -35,3 +48,35 @@ class ChicagoCrimesDAL:
         cursor = self.__session.query(query)
 
         return tuple(one(cursor))
+
+    def crime_locations(self, date_range: DateRange, crime_types: list[str]) -> list[Location]:
+        """
+        Returns a list of crime locations
+        >>> range = DateRange(datetime.date(2022, 10, 1), datetime.date(2022, 10, 18))
+        >>> ChicagoCrimesDAL(bigquery.Client()).crime_locations(range, )
+
+        :param date_range: range of dates to query by
+        :param crime_types: list of crime types to query by
+        :return: list of locations
+        """
+
+        query = '''        
+            SELECT latitude,
+                longitude
+            FROM `bigquery-public-data.chicago_crime.crime`
+            WHERE x_coordinate IS NOT NULL
+              AND y_coordinate IS NOT NULL
+              AND DATE(date) BETWEEN @start_date AND @end_date
+              AND primary_type in UNNEST(@crime_types);
+        '''
+        job_config = bigquery.QueryJobConfig(
+            query_parameters=[
+                bigquery.ScalarQueryParameter('start_date', 'DATE', date_range.start_date),
+                bigquery.ScalarQueryParameter('end_date', 'DATE', date_range.end_date),
+                bigquery.ArrayQueryParameter('crime_types', 'STRING', crime_types),
+            ]
+        )
+
+        cursor = self.__session.query(query, job_config=job_config)
+
+        return [Location(*row) for row in cursor]
